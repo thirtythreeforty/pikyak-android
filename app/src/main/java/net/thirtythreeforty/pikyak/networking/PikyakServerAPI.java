09076b4bd6a7ae6c826f80f7967d0a1488ac452c
George Hilliard <gh403@msstate.edu>
2014-10-21 16:52:23 -0500

    Make API asynchronous

--- app/src/main/java/net/thirtythreeforty/pikyak/networking/PikyakServerAPI.java
+++ app/src/main/java/net/thirtythreeforty/pikyak/networking/PikyakServerAPI.java
@@ -1,7 +1,13 @@
 package net.thirtythreeforty.pikyak.networking;
 
-import net.thirtythreeforty.pikyak.networking.model.*;
+import net.thirtythreeforty.pikyak.networking.model.ConversationListModel;
+import net.thirtythreeforty.pikyak.networking.model.ConversationModel;
+import net.thirtythreeforty.pikyak.networking.model.PostModel;
+import net.thirtythreeforty.pikyak.networking.model.RegistrationBodyModel;
+import net.thirtythreeforty.pikyak.networking.model.RegistrationResponseModel;
+import net.thirtythreeforty.pikyak.networking.model.UnregistrationResponseModel;
 
+import retrofit.Callback;
 import retrofit.http.Body;
 import retrofit.http.DELETE;
 import retrofit.http.GET;
@@ -10,18 +16,34 @@ import retrofit.http.Path;
 import retrofit.http.Query;
 
 public interface PikyakServerAPI {
+    public static String SORT_METHOD_HOT = "hot";
+
     @PUT("/users/{username}")
-    RegistrationResponseModel register(@Path("username") String username, @Body RegistrationBodyModel body);
+    public void register(
+            @Path("username") String username,
+            @Body RegistrationBodyModel body,
+            Callback<RegistrationResponseModel> callback);
 
     @DELETE("/users/{username}")
-    UnregistrationResponseModel unregister(@Path("username") String username);
+    public void unregister(
+            @Path("username") String username,
+            Callback<UnregistrationResponseModel> callback);
 
     @GET("/conversations")
-    ConversationListModel getConversationList(@Query("first") int first_conversation, @Query("sort") String sort_method, @Query("geo") String lat_and_long);
+    public void getConversationList(
+            @Query("first") int first_conversation,
+            @Query("sort") String sort_method,
+            @Query("geo") String lat_and_long,
+            Callback<ConversationListModel> callback);
 
     @GET("/conversation/{conversation_id}")
-    ConversationModel getConversation(@Path("conversation_id") int conversation_id, @Query("first") int first_post);
+    public void getConversation(
+            @Path("conversation_id") int conversation_id,
+            @Query("first") int first_post,
+            Callback<ConversationModel> callback);
 
     @GET("/post/{post_id}")
-    PostModel getPost(@Path("post_id") int post_id);
+    public void getPost(
+            @Path("post_id") int post_id,
+            Callback<PostModel> callback);
 }
